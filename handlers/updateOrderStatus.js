7ee35df1259ca00f5ebdc665c4088f1185742fac
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-06-16 18:57:21 +0530

    pcakages, step function config and update order status handler added

---
+++ handlers/updateOrderStatus.js
@@ -0,0 +1,35 @@
+const { DynamoDBClient, UpdateItemCommand } = require('@aws-sdk/client-dynamodb');
+
+const dynamoDBClient = new DynamoDBClient({ region: process.env.REGION });
+
+exports.updateOrderStatus = async (event) => {
+	try {
+		const { id } = event;
+
+		await dynamoDBClient.send(
+			new UpdateItemCommand({
+				TableName: process.env.ORDER_TABLE,
+				Key: {
+					id: { S: id },
+				},
+				UpdateExpression: 'SET #status = :newStatus',
+				ExpressionAttributeNames: {
+					'#status': 'status',
+				},
+				ExpressionAttributeValues: {
+					':newStatus': { S: 'PROCESSING' },
+				},
+			})
+		);
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({ message: 'Order status updated successfully.' }),
+		};
+	} catch (error) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ error: 'Error updating order status', details: error.message }),
+		};
+	}
+};
